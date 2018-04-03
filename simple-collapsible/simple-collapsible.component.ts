17d8de4dbf0de5be247553659e1f6de07b806b08
André Porto <andre.porto@serpro.gov.br>
2018-04-03 11:02:46 -0300

    refactoring

--- simple-collapsible/simple-collapsible.component.ts
+++ simple-collapsible/simple-collapsible.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, trigger, transition, style, animate } from '@angular/core';
+import { Component, OnInit, trigger, transition, style, animate, Input } from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -27,21 +27,26 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     )
   ],
   template: `
-  <div class="clickableCursor" (click)="onClick($event)" [ngClass]="{'expanded':expand}">
+  <div class="clickableCursor relative" (click)="onClick($event)" [ngClass]="{'expanded':expand}">
+    <div *ngIf="expand && iconLocation != 'none'" class="expandIcon  sc-clickable" [ngClass]="{'in':iconLocation=='in','out':iconLocation=='out','top':iconAlign=='top','middle':iconAlign=='middle','bottom':iconAlign=='bottom'}"><i class="icon-check_minus sc-clickable"></i></div>
+    <div *ngIf="!expand && iconLocation != 'none'" class="expandIcon sc-clickable" [ngClass]="{'in':iconLocation=='in','out':iconLocation=='out','top':iconAlign=='top','middle':iconAlign=='middle','bottom':iconAlign=='bottom'}"><i class="icon-plus-square-o sc-clickable"></i></div>
     <ng-content select="[sc-header]"></ng-content>
   </div>
-  <div *ngIf="expand" [@myAnimation]>
+  <div *ngIf="expand" [@myAnimation] class="relative">
     <ng-content select="[sc-content]"  ></ng-content>
   </div>
   `,
 })
 export class SimpleCollapsibleComponent implements OnInit {
 
-  expand = false;
+  @Input()iconAlign:string = 'middle'; // left;right;top;middle;bottom
+  @Input()iconLocation:string = 'in'; // in;out;none
+  @Input()expand:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
   onClick(ev) {
